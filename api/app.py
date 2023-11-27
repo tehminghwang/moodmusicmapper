7d2798e3ab142b63aeb2d6f893e31f3eca2a5f93
Ming Hwang Teh <ming.teh09@imperial.ac.uk>
2023-11-27 17:55:17 +0000

    new

--- api/app.py
+++ api/app.py
@@ -8,5 +8,22 @@ def hello_world():
 
 @app.route("/submit", methods=["POST"])
 def submit():
-    input_mood = request.form.get("mood")
-    return render_template("mood.html", mood=input_mood)
+    input_mood = "ONLY generate values for valency, danceability and enegery based on the mood: " + request.form.get("mood")
+    reply = send_request(input_mood)
+    return render_template("mood.html", mood=reply)
+
+
+def send_request(prompt):
+    api_key = "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"  # Replace with your OpenAI API key
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "model": "gpt-4.0-turbo",  # or another model version
+        "prompt": prompt,
+        "max_tokens": 150
+    }
+    response = requests.post("https://api.openai.com/v1/engines/gpt-4.0-turbo/completions", json=payload, headers=headers)
+    return response.json()
+
