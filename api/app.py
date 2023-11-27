cefc57401e71d832afcf1dd4972fa329e4b676dd
Ming Hwang Teh <ming.teh09@imperial.ac.uk>
2023-11-27 18:56:02 +0000

    new

--- api/app.py
+++ api/app.py
@@ -1,33 +1,31 @@
 from flask import Flask, render_template, request
-#import openai
-#import os
-#import requests
+import openai
+import os
 app = Flask(__name__)
 
-#openai.api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 #client = openai.Client()
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
 
+def send_request(mood):
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-004",
+            prompt=f"ONLY generate values for valency, danceability, and energy based on the mood: {mood}",
+            max_tokens=60
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        return f"Error: {e}"
 
 @app.route("/submit", methods=["POST"])
 def submit():
     input_mood = request.form.get("mood")
-    #input_mood = "ONLY generate values for valency, danceability and enegery based on the mood: " + request.form.get("mood")
-    #reply = send_request(input_mood)
-    #return render_template("mood.html", mood=reply)
-    return render_template("mood.html", mood=input_mood)
+    reply = send_request(input_mood)
+    return render_template("mood.html", mood=reply)
 
-
-#def send_request(prompt):
-    #response = client.Completion.create(
-     #   model="text-davinci-003",  # Replace with your desired model
-        #prompt=prompt,
-        #temperature=0.7,
-        #max_tokens=150
-    #)
-    #return response
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
