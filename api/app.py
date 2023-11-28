25d2ab6ab7342f0eaa593ab7fc6f44922953d5ca
Ming Hwang Teh <ming.teh09@imperial.ac.uk>
2023-11-28 13:17:47 +0000

    new

--- api/app.py
+++ api/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
 from openai import OpenAI
 import os
+from spotify import spotify_main
+from ipfinder import get_city_from_ip
+import re
 app = Flask(__name__)
 
 
 client = OpenAI(
     # defaults to os.environ.get("OPENAI_API_KEY")
+    #api_key=""
     api_key=os.environ.get('OPENAI_API_KEY')
     #api_key=os.environ.get("API_URL")
 )
@@ -23,7 +27,7 @@ def send_request(mood):
             messages=[
                 {'role': 'system', 'content': 'You are a helpful assistant.'},
                 {'role': 'user',
-                 'content': f'I am feeling {mood}. Can you suggest music characteristics like valency (value from 0.0 to 1.0 with 1.0 being more positive (e.g. happy, cheerful, euphoric)), danceability (value of 0.0 is least danceable and 1.0 is most danceable based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity), and energy (from 0.0 to 1.0 and represents a perceptual measure of intensity and activity) that would suit my mood, and describe my mood in 1 word? Reply only with three values and a word.'}
+                 'content': f'I am feeling {mood}. Can you suggest music characteristics like valency (value from 0.0 to 1.0 with 1.0 being more positive (e.g. happy, cheerful, euphoric)), danceability (value of 0.0 is least danceable and 1.0 is most danceable based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity), and energy (from 0.0 to 1.0 and represents a perceptual measure of intensity and activity) that would suit my mood, and describe my mood in 1 word, and recommend 1 song to suit my mood? Reply as [Valency: ??] [Danceability: ??] [Energy: ??] [Mood: ??] [Song: ??] [Singer: ??].'}
             ],
             max_tokens=60
         )
@@ -37,7 +41,39 @@ def send_request(mood):
 def submit():
     input_mood = request.form.get("mood")
     reply = send_request(input_mood)
-    return render_template("mood.html", mood=reply)
+    valency, danceability, energy, mood, song, singer = extract_values(reply)
+    response = f"Valency: {valency}, Danceability: {danceability}, Energy: {energy}, Mood: {mood}, Song: {song}, Singer: {singer}"
+    playlist = spotify_main(valency, danceability, energy)
+    city = get_city_from_ip()
+    return render_template("mood.html", mood=playlist, response=response, reply=reply, city=city)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+def extract_values(text):
+    # Regular expressions to extract the values
+    valency_pattern = r"Valency:\s([0-9.]+)"
+    danceability_pattern = r"Danceability:\s([0-9.]+)"
+    energy_pattern = r"Energy:\s([0-9.]+)"
+    mood_pattern = r"Mood:\s(\w+)(?!.*Mood:)"
+    song_pattern = r"Song:\s\"([^\"]+)\""
+    singer_pattern = r"Singer:\s([^\]]+)"
+
+    # Extracting values
+    valency = re.search(valency_pattern, text)
+    danceability = re.search(danceability_pattern, text)
+    energy = re.search(energy_pattern, text)
+    mood = re.search(mood_pattern, text)
+    song = re.search(song_pattern, text)
+    singer = re.search(singer_pattern, text)
+
+    # Assigning to variables and converting to appropriate types
+    valency = float(valency.group(1)) if valency else None
+    danceability = float(danceability.group(1)) if danceability else None
+    energy = float(energy.group(1)) if energy else None
+    mood = mood.group(1) if mood else None
+    song = song.group(1) if song else None
+    singer = singer.group(1) if singer else None
+
+    return valency, danceability, energy, mood, song, singer
+
--- api/spotify.py
+++ api/spotify.py
@@ -13,7 +13,7 @@ def get_spotify_access_token(client_id, client_secret):
     access_token = response.json().get('access_token')
     return access_token
 
-def get_spotify_recommendations(access_token, valence, danceability, energy):
+def get_spotify_recommendations(access_token, seed_genre, valence, danceability, energy):
     # Get recommendations based on input parameters
     recommendations_url = 'https://api.spotify.com/v1/recommendations'
     headers = {'Authorization': f'Bearer {access_token}'}
@@ -41,21 +41,23 @@ def get_spotify_recommendations(access_token, valence, danceability, energy):
 
     return tracks
 
-if __name__ == "__main__":
-    # Replace 'YOUR_CLIENT_ID' and 'YOUR_CLIENT_SECRET' with your actual Spotify API credentials
-    client_id = 'bc4a63dca78b417db515f5b70813b986'
+def spotify_main(valence, danceability, energy):    # Replace 'YOUR_CLIENT_ID' and 'YOUR_CLIENT_SECRET' with your actual Spotify API credentials
+    #client_id = ''
     client_secret = os.environ.get('SPOTIFY_KEY')
+    client_secret = 'd5cc981f4fc84e0d82aea910397645df'
 
     access_token = get_spotify_access_token(client_id, client_secret)
 
     # Replace these values with the desired input parameters
     seed_genre = 'pop' #seed artist, genres, tracks: at least one required
-    valence = 0.8  # Range: 0.0 to 1.0
-    danceability = 0.7  # Range: 0.0 to 1.0
-    energy = 0.8  # Range: 0.0 to 1.0
+    #valence = 0.8  # Range: 0.0 to 1.0
+    #danceability = 0.7  # Range: 0.0 to 1.0
+    #energy = 0.8  # Range: 0.0 to 1.0
 
-    recommendations = get_spotify_recommendations(access_token, valence, danceability, energy)
+    recommendations = get_spotify_recommendations(access_token, seed_genre, valence, danceability, energy)
+
+    return recommendations
 
     # Print the recommendations
-    for i, track in enumerate(recommendations, start=1):
-        print(f"{i}. {track['name']} by {track['artist']} (URI: {track['uri']})")
+    #for i, track in enumerate(recommendations, start=1):
+        #print(f"{i}. {track['name']} by {track['artist']} (URI: {track['uri']})")
