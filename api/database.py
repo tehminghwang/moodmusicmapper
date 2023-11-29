0b2cea1aea94d11119fb5eb1d5acb9df6b749e4d
Han Lin Lee <hanlinlee@dyn3201-16.wlan.ic.ac.uk>
2023-11-29 11:38:15 +0000

    Database

--- api/database.py
+++ api/database.py
@@ -1,16 +1,15 @@
 import psycopg as db
 import configparser
-from api import dbtool.ini
 
 def insert_into_table(valency, dance, energy, summary, ip_address, city, country):
     # read in configuration file parameters from dbtool.ini
     config = configparser.ConfigParser()
-    config.read(’dbtool.ini’)
+    config.read('dbtool.ini')
 
-    conn = db.connect(∗∗config[’connection’])
+    conn = db.connect(**config['connection'])
     curs = conn.cursor()
 
-    curs.execute(”INSERT INTO mood VALUES (%s, %s, %s, %s, TIMESTAMP(NOW()), %s)”, (valency, dance, energy, summary, ip_address))
-    curs.execute(”INSERT INTO location VALUES (%s, %s, %s)”, (ip_address, city, country))
+    curs.execute("INSERT INTO mood VALUES (%s, %s, %s, %s, TIMESTAMP(NOW()), %s)", (valency, dance, energy, summary, ip_address))
+    curs.execute("INSERT INTO location VALUES (%s, %s, %s)", (ip_address, city, country))
 
     conn.close()
