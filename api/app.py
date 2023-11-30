370471146d6171ab33b2909e434b30775961a6f4
Ming Hwang Teh <ming.teh09@imperial.ac.uk>
2023-11-30 12:26:08 +0000

    new

--- api/app.py
+++ api/app.py
@@ -1,21 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from openai import OpenAI
 import os
 import re
-from api import spotify_mod, database, ipfinder
-#import spotify_mod, database, ipfinder
+import folium
+import json
+from branca.colormap import LinearColormap
+#from api import spotify_mod, database, ipfinder
+import spotify_mod, database, ipfinder
+import geopy
+from geopy.geocoders import Nominatim
 app = Flask(__name__)
 
 
 client = OpenAI(
-    #api_key=""
-    api_key=os.environ.get('OPENAI_API_KEY')
+    api_key="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"
+    #api_key=os.environ.get('OPENAI_API_KEY')
     #api_key=os.environ.get("API_URL")
 )
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    playlist_cookie = request.cookies.get('playlist')
+    saved_playlist = None  # Initialize saved_playlist to None
+    if playlist_cookie:
+        try:
+            saved_playlist = json.loads(playlist_cookie)
+        except json.JSONDecodeError:
+            # Handle the case where the cookie contains invalid JSON
+            pass
+
+    return render_template("index.html", mood=saved_playlist)
 
 def send_request(mood):
     try:
@@ -44,6 +58,63 @@ def send_request(mood):
     except Exception as e:
         return f"Error: {e}"
 
+# Placeholder for mood data
+mood_data = {
+    "London": {"mood": "Happy", "song": "Here Comes the Sun", "index": 0.2},
+    "New York": {"mood": "Energetic", "song": "Here Comes the Sun", "index": 0.8},
+    "California": {"mood": "Energetic", "song": "Here Comes the Sun", "index": 0.5},
+    "Berlin": {"mood": "Energetic", "song": "Here Comes the Sun", "index": 0.4},
+    "Beijing": {"mood": "Energetic", "song": "Here Comes the Sun", "index": 0.3}
+}
+
+def create_colormap():
+    colors = ['green', 'yellow', 'red']
+    index = [0, 0.5, 1]  # Points at which colors change
+    return LinearColormap(colors, vmin=min(index), vmax=max(index))
+
+
+def create_map(mood_data):
+    # Start with a world map
+    m = folium.Map(location=[20, 0], zoom_start=2)
+
+    # Define a linear color map
+    colormap = create_colormap()
+
+    for city, info in mood_data.items():
+        lat, lon = get_coordinates(city)
+        if lat is not None and lon is not None:
+            # Get color from the colormap
+            color = colormap(info['index'])
+
+            # Customize the popup with HTML content
+            html_content = f"""
+            <div style="width: 250px; height: 150px;">
+                <strong>{city}</strong><br>
+                Mood: {info['mood']}<br>
+                Song: {info['song']}<br>
+                <iframe src="https://open.spotify.com/embed/track/6kex4EBAj0WHXDKZMEJaaF" width="250" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            </div>
+            """
+            iframe = folium.IFrame(html_content, width=270, height=200)
+            popup = folium.Popup(iframe, parse_html=False)
+
+            # Add a Circle marker with the color
+            folium.Circle(
+                location=[lat, lon],
+                radius=300000,  # Adjust the radius as needed
+                color=color,
+                fill=True,
+                fill_color=color,
+                fill_opacity=1,
+                popup=popup
+            ).add_to(m)
+
+    # Optionally, add the colormap to the map for reference
+    colormap.add_to(m)
+
+    return m
+
+
 @app.route("/submit", methods=["POST"])
 def submit():
     input_mood = request.form.get("mood")
@@ -51,13 +122,33 @@ def submit():
     valency, danceability, energy, mood, song, singer = extract_values(reply)
     response = f"Valency: {valency}, Danceability: {danceability}, Energy: {energy}, Mood: {mood}, Song: {song}, Singer: {singer}"
     playlist = spotify_mod.spotify_main(valency, danceability, energy)
+    playlist_json = json.dumps(playlist)
+    #response.set_cookie('playlist', playlist, max_age=60 * 60 * 24 * 30)  # Cookie expires in 30 days
     city = ipfinder.get_city_from_ip()
+
+    # Generate the map with mood data
+    folium_map = create_map(mood_data)
+    map_html = folium_map._repr_html_()
+
     #country=time=cookies = "123abc" # temp placeholder
     #insert_into_database(cookies, valency, danceability, energy, mood, time, ipaddress, city, country)
-    return render_template("mood.html", mood=playlist, response=response, reply=reply, city=city)
+    response_html = render_template("mood.html", mood=playlist, response=response, reply=reply, city=city, map_html=map_html)
+    # Create a response object from the rendered HTML
+    response = make_response(response_html)
+    # Set a cookie in the response object
+    response.set_cookie('playlist', playlist_json, max_age=60 * 60 * 24 * 30)  # Cookie expires in 30 days
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+def get_coordinates(city_name):
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    location = geolocator.geocode(city_name)
+    if location:
+        return location.latitude, location.longitude
+    else:
+        return None, None
 
 def extract_values(text):
     # Regular expressions to extract the values
