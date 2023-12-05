5ef90b9e44ff03201a7127aaf315e2e8a38bf3ad
Ming Hwang Teh <ming.teh09@imperial.ac.uk>
2023-12-05 13:35:36 +0000

    new

--- api/app_test.py
+++ api/app_test.py
@@ -1,5 +1,6 @@
 import json
-from app import hello_world
+import app
+#from app import hello_world
 from flask import request, render_template
 from unittest.mock import patch, MagicMock
 
@@ -8,7 +9,7 @@ from unittest.mock import patch, MagicMock
 def test_hello_world_no_cookie():
     with app.test_request_context('/'):
         with patch('your_module.render_template') as mock_render:
-            hello_world()
+            app.hello_world()
             mock_render.assert_called_once_with("index.html", mood=None)
 
 # Test with valid cookie
@@ -17,7 +18,7 @@ def test_hello_world_valid_cookie():
     with app.test_request_context('/?playlist=' + valid_playlist):
         with patch.object(app, 'cookies', new_callable=MagicMock(return_value={'playlist': valid_playlist})):
             with patch('your_module.render_template') as mock_render:
-                hello_world()
+                app.hello_world()
                 mock_render.assert_called_once_with("index.html", mood=json.loads(valid_playlist))
 
 # Test with invalid cookie
@@ -25,5 +26,5 @@ def test_hello_world_invalid_cookie():
     with app.test_request_context('/?playlist=invalid json'):
         with patch.object(app, 'cookies', new_callable=MagicMock(return_value={'playlist': 'invalid json'})):
             with patch('your_module.render_template') as mock_render:
-                hello_world()
+                app.hello_world()
                 mock_render.assert_called_once_with("index.html", mood=None)
