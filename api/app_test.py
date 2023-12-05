cd2106ec9a7788c96ce30692fbb0e9cd00606750
Ming Hwang Teh <ming.teh09@imperial.ac.uk>
2023-12-05 16:24:46 +0000

    new

--- api/app_test.py
+++ api/app_test.py
@@ -14,13 +14,13 @@ def test_hello_world_no_cookie():
 
 
 # Test with valid cookie
-#def test_hello_world_valid_cookie():
-#    valid_playlist = '[{"name": "Christmas Lights", "artist": "Coldplay", ...}]'
-#    with app.test_request_context('/'):
-#        with patch('flask.request.cookies', new=MagicMock(return_value={'playlist': valid_playlist})):
-#            with patch('flask.render_template') as mock_render:
-#                app.hello_world()
-#                mock_render.assert_called_once_with("index.html", mood=json.loads(valid_playlist))
+def test_hello_world_valid_cookie():
+    valid_playlist = '[{"name": "Christmas Lights", "artist": "Coldplay"}]'
+    with app.test_client() as client:
+        with patch('app.render_template') as mock_render:
+            response = client.get('/')
+            mock_render.assert_called_once_with("index.html", mood=json.loads(valid_playlist))
+
 
 # Test with invalid cookie
 #def test_hello_world_invalid_cookie():
